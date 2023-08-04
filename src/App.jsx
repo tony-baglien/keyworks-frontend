689cf8330d774060ca9efacfcee5dc4db38f9205
Tony Baglien <tbaglien@crescendocollective.com>
2023-08-04 15:35:50 -0500

    initialized react with vite and added in font hosting

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Test from "./components/test";
+
+export default function App() {
+    return (
+        <>
+            <Test />
+        </>
+    );
+}
---
+++ src/components/test.jsx
@@ -0,0 +1,7 @@
+import "./test.scss";
+
+const Test = () => {
+    return <div className="test">test</div>;
+};
+
+export default Test;
